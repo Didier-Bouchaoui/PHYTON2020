2555908512b8a33193ba9d2c1ac69eaf8301e30a
Didier Bouchaoui <38864233+Didier-Bouchaoui@users.noreply.github.com>
2020-12-04 17:05:58 +0100

    Add files via upload
    première partie de l'étude d'API avec une classification de tweet

---
+++ Pipline_TP/Tweet_Pipeline.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# TP pipline_Classification
+# 
+
+# Etape 1 import et clean
+# 
+
+# In[15]:
+
+
+import pandas as pds
+import numpy as np
+import re
+import pickle
+from sklearn import svm
+from sklearn.svm import SVC
+from sklearn.pipeline import make_pipeline
+from sklearn.naive_bayes import GaussianNB
+from sklearn import datasets
+import sklearn as skl
+from sklearn.feature_extraction.text import TfidfVectorizer
+from stop_words import get_stop_words
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.model_selection import train_test_split
+
+
+# ## Mise en place de la classification
+# 
+
+# In[16]:
+
+
+clf = svm.SVC()
+X, y= datasets.load_iris(return_X_y=True)
+clf.fit(X, y)
+
+
+# In[17]:
+
+
+clf=SVC()
+
+
+# In[18]:
+
+
+df = pds.read_csv("dataset/labels.csv", usecols =["class", "tweet"])
+
+
+# In[19]:
+
+
+df.head(10)
+
+
+# In[20]:
+
+
+
+df['tweet'] = df['tweet'].apply(lambda tweet: re.sub('[^A-Za-z]+', ' ', tweet.lower()))
+
+
+# In[21]:
+
+
+df
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[22]:
+
+
+clf = make_pipeline(
+    TfidfVectorizer(stop_words=get_stop_words('en')),
+    OneVsRestClassifier(SVC(kernel='linear', probability=True)))
+
+
+# In[23]:
+
+
+X = df['tweet']
+y = df['class']
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.80, random_state=20)
+clf.fit(X=X_train, y=y_train)
+
+
+# In[24]:
+
+
+res = pickle.dumps(clf)
+clf2 = pickle.loads(res)
+
+
+# In[25]:
+
+
+clf2.predict(df['tweet'][0:15])
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
